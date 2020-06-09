1e1c3aa511ee4a6a7fe0193d2e7f7e01898e6768
ShionWebGym <a18dc208@dhw.ac.jp>
2020-06-10 04:36:45 +0900

    waku kansei??

--- src/Others.js
+++ src/Others.js
@@ -4,31 +4,35 @@ class Others extends Component{
     render(){
         return(
             <div className="others">
-            <h2>OTHER SUBJECTS</h2>
-            <ul>
-                <li>
-                    <a href="https://www.dhw.ac.jp/faculty/3dcg/">3DCG・VFX</a>
+            <h2 className="others-head">OTHER SUBJECTS</h2>
+            <ul className="others-box">
+                <li className="grid1">
+                    <a href="https://www.dhw.ac.jp/faculty/3dcg/" className="grid-image_3dcg"><span className="grid-text">3DCG・VFX</span></a>
                 </li>
-                <li>
-                    <a href="https://www.dhw.ac.jp/faculty/game/">ゲームプログラミング</a>
+                <li className="grid2">
+                    <a href="https://www.dhw.ac.jp/faculty/game/" className="grid-image_game"><span className="grid-text">ゲームプログラミング</span></a>
                 </li>
-                <li>
-                    <a href="https://www.dhw.ac.jp/faculty/movie/">映像・映画</a>
+                <li className="grid3">
+                    <a href="https://www.dhw.ac.jp/faculty/movie/" className="grid-image_mov"><span className="grid-text">映像・映画</span></a>
                 </li>
-                <li>
-                    <a href="https://www.dhw.ac.jp/faculty/graphic/">グラフィックデザイン</a>
+                <li className="grid4">
+                    <a href="https://www.dhw.ac.jp/faculty/graphic/" className="grid-image_gra"><span className="grid-text">グラフィックデザイン</span></a>
                 </li>
-                <li>
-                    <a href="https://www.dhw.ac.jp/faculty/anime/">アニメ</a>
+                <li className="grid5">
+                    <a href="https://www.dhw.ac.jp/faculty/anime/" className="grid-image_ani"><span className="grid-text">アニメ</span></a>
                 </li>
-                <li>
-                    <a href="https://www.dhw.ac.jp/faculty/media/">VR/AR メディアアート</a>
+                <li className="grid6">
+                    <a href="https://www.dhw.ac.jp/faculty/web/" className="grid-image_web"><span className="grid-text">Webデザイン・Web開発</span></a>
                 </li>
-                <li>
-                    <a href="https://www.dhw.ac.jp/faculty/business/">広告・起業</a>
+                <li className="grid7">
+                    <a href="https://www.dhw.ac.jp/faculty/media/" className="grid-image_med"><span className="grid-text">VR/AR メディアアート</span></a>
+                </li>
+                <li className="grid8">
+                    <a href="https://www.dhw.ac.jp/faculty/business/" className="grid-image_bus"><span className="grid-text">広告・起業</span></a>
                 </li>
             </ul>
-            <a href="#top">サイトトップへ戻る</a>
+            <a href="#top" className="backbtn">サイトトップへ戻る</a>
+            <p className="copy"><small>Copyright c 2018 Digital Hollywood Co.,Ltd. All Rights Reserved.</small></p>
             </div>
         );
     }
